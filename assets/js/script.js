7d70cda5146e17d758aca7fdcc62d39b40e73436
drognax <dognax@hotmail.com>
2022-04-28 19:24:33 -0400

    consulta inicial superhero

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#buscador').click(function(){
+        let personaje = document.getElementById('personaje').value;
+        console.log(personaje);
+        $.ajax({
+            type: "GET",
+            url: "https://www.superheroapi.com/api.php/10224452404358844/"+personaje,
+            dataType: "json",
+            success: function (res){
+                console.log(res);
+            },
+            error: function (res){
+                alert("error");
+            }
+        })
+    }
+    )
+});
